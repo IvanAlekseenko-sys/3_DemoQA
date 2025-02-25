bc87d815fac5a2bff027e7dbfc40d90c1570b5c8
Ivan <ivanalekseenko.post@gmail.com>
2025-02-25 10:55:49 +0100

    code upd

--- src/test/java/demoqa/forms/PracticeFormTest.java
+++ src/test/java/demoqa/forms/PracticeFormTest.java
@@ -5,6 +5,7 @@ import demoqa.pages.HomePage;
 import demoqa.pages.PracticeFormPage;
 import demoqa.pages.SidePanel;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class PracticeFormTest extends TestBase {
@@ -24,11 +25,31 @@ public class PracticeFormTest extends TestBase {
                 .enterSubjects(new String[]{"Maths", "English"})
                 .chooseHobbies(new String[]{"Sports", "Music"})
                 .uploadPicture("C:\\Users\\Ivan\\Downloads\\unnamed2.png")
-        // .enterCurrentAddress("Transylvania, Romania")
-        //.enterState("NCR")
-        //.enterCity("Bucharest")
-        // .submitForm()
-        // .verifySuccessRegistration("Thanks for submitting the form")
+                .enterCurrentAddress("Transylvania, Romania")
+                .enterState("NCR")
+                .enterCity("Delhi")
+                .submitForm()
+                .verifySuccessRegistration("Thanks for submitting the form")
+        ;
+
+    }
+
+    @Test
+    @Parameters({"firstName", "lastName", "email", "telNumber"})
+    public void practiceFormParameterPositiveTest(String firstName, String lastName, String email, String telNumber) {
+        new PracticeFormPage(app.driver, app.wait)
+                .enterPersonalData(firstName, lastName, System.currentTimeMillis()+email, telNumber)
+                .selectGender("Male")
+                .chooseBirthDateAsString("10 Jan 1450")
+                //.chooseDate("10", "January", "1910")
+                .enterSubjects(new String[]{"Maths", "English"})
+                .chooseHobbies(new String[]{"Sports", "Music"})
+                .uploadPicture("C:\\Users\\Ivan\\Downloads\\unnamed2.png")
+                .enterCurrentAddress("Transylvania, Romania")
+                .enterState("NCR")
+                .enterCity("Delhi")
+                .submitForm()
+                .verifySuccessRegistration("Thanks for submitting the form")
         ;
 
     }
